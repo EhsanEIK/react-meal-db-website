5d2bdbd287c18e2794fd7b6677ad088ab9eb5346
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-09-27 11:26:37 +0600

    added footer & favicon

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Body from './components/Body/Body';
+import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div>
       <Header></Header>
       <Body></Body>
+      <Footer></Footer>
     </div>
   );
 }
