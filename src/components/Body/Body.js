669f19fe7d13b78eef8656d711971388bfa7eae8
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-09-28 11:05:58 +0600

    added food search option

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -7,14 +7,21 @@ import './Body.css'
 const Body = () => {
     const [meals, setMeals] = useState([]);
     const [cart, setCart] = useState([]);
+    const [search, setSearch] = useState([]);
+
+    // for searching food by button
+    const searchFoodByButton = () => {
+        const searchValue = document.getElementById('search-element').value;
+        setSearch(searchValue);
+    }
 
     useEffect(() => {
-        fetch('https://www.themealdb.com/api/json/v1/1/search.php?f=a')
+        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`)
             .then(res => res.json())
             .then(data => setMeals(data.meals));
-    }, []);
+    }, [search]);
 
-    // save data to local storage and display selected meal on UI when click on the button 
+    // save data to local storage and display selected meal on UI when click on the button
     const addToCart = (selectedMeal) => {
         let newCart = [];
         const exists = cart.find(m => m.idMeal === selectedMeal.idMeal);
@@ -43,23 +50,29 @@ const Body = () => {
             }
         }
         setCart(newCart);
-    }, [meals])
+    }, [meals]);
 
-    // console.log(cart);
     return (
         <div className='main-body'>
-            <div className='meal-container'>
-                {
-                    meals.map(meal => <Meal
-                        key={meal.idMeal}
-                        meal={meal}
-                        handleMealCart={addToCart}></Meal>)
-                }
+            <div>
+                <div className='search-container'>
+                    <input onChange={(e) => setSearch(e.target.value)} type="text" id="search-element" />
+                    <button onClick={searchFoodByButton} className='btn-search'>search</button>
+                </div>
+                <div className='meal-container'>
+                    {
+                        meals ? meals.map(meal => <Meal
+                            key={meal.idMeal}
+                            meal={meal}
+                            handleMealCart={addToCart}></Meal>)
+                            : <p className='warning-message'>No Items Found</p>
+                    }
+                </div>
             </div>
             <div className='cart-container'>
                 <Cart cart={cart}></Cart>
             </div>
-        </div>
+        </div >
     );
 };
 
