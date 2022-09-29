8c98acfd004cf8808e1e7eb08e42b2497205f11d
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-09-29 17:36:01 +0600

    added toastify in 'confirm order' button

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './Cart.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Cart = ({ cart }) => {
     // console.log(cart);
@@ -7,6 +9,10 @@ const Cart = ({ cart }) => {
     for (const meal of cart) {
         quantity += meal.quantity;
     }
+
+    // toastify message
+    const notify = () => toast.success("Your order is on the way!")
+
     return (
         <div className='cart'>
             <h1>Meal Cart</h1>
@@ -14,7 +20,8 @@ const Cart = ({ cart }) => {
             {
                 cart.map(meal => <li key={meal.idMeal}>{meal.strMeal} {meal.quantity ? meal.quantity : ''}</li>)
             }
-            <button className='btn-confirm'>Confirm Order</button>
+            <button onClick={notify} className='btn-confirm'>Confirm Order</button>
+            <ToastContainer></ToastContainer>
         </div>
     );
 };
